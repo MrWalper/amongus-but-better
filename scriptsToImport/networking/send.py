778ea87ceaf6af476a6479a60d264ddd131fbc2c
MrWalper <verywalper@gmail.com>
2023-08-02 19:30:06 +0200

    Updated comment in send.py

--- scriptsToImport/networking/send.py
+++ scriptsToImport/networking/send.py
@@ -7,6 +7,9 @@ packetId:data1:data2 etc.
 The order of the data will be determined by order that it does by in packet.py
 If there is no data to send (for example in keepAlivePacket)
 then add 0 after packetId
+
+If you want to send coords do it like this:
+[x,y,z]
 """
 
 config = openJson("system-files\config.json")["logpackets"]
